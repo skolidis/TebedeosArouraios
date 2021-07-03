bf45998537bc2c31e4e985c995060dc6e268b91b
ntsiouma <ntsiouma@csd.auth.gr>
2021-07-03 20:01:27 +0300

    clean

--- src/Rea.java
+++ src/Rea.java
@@ -106,24 +106,6 @@ public class Rea {
         return near;
     }
 
-    public double minDist(node Point){
-        float min=0;
-        float rd;
-        int n=Point.getDims();
-
-        for (int i=0; i<n;i++){
-            if (boundlow[i]>n){
-                rd=boundlow[i];
-            }else if (boundhigh[i]<n){
-                rd=boundhigh[i];
-            }else{
-                rd=n;
-            }
-            min+=Math.pow(n-rd,2);
-        }
-        return sqrt(min);
-    }
-
     public double Mk(int n, double s, double t){
         double sum=(s + t)/2;
         if(n<=sum) {
@@ -172,24 +154,6 @@ public class Rea {
             sum+=Math.pow(A[i]-B[i],2);
         }
         return sqrt(sum);
-    }
-
-    public ArrayList<Rea> minDistRea(node Point){
-        float min=0;
-        ArrayList<Rea> near=nodes;
-        int n=Point.getDims(),i=0;
-        float[] coord= Point.getCoordinates();
-        float minLow=boundlow[i],minHigh=boundhigh[i];
-
-        for (i=0; i<n;i++){
-            if (minLow<boundlow[i] && boundhigh[i]>minHigh){
-                minLow=boundlow[i];
-                minHigh=boundhigh[i];
-                near=nodes;
-            }
-
-        }
-        return near;
     }
 
     private void removeLeaves(int[] ids){
